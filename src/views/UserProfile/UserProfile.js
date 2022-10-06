28a7817bc627ce4c9b50ab8449e5c66a8cefd123
thanh-nguyen03 <nguyenducthanh240903@gmail.com>
2022-10-06 11:47:56 +0700

    add edit profile and change password logic

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -1,18 +1,139 @@
-import React, { useState } from 'react';
+import { getCollectorInformation, updateCollectorInformation } from 'api/http';
+import React, { useEffect, useState } from 'react';
+import Loader from 'UI/Loader';
+import Modal from 'UI/Modal';
+import Swal from 'sweetalert2';
 
 export default function UserProfile() {
   const [isChangingPassword, setIsChangingPassword] = useState(false);
   const [isValidated, setIsValidated] = useState(false);
+  const [profileInput, setProfileInput] = useState({
+    fullname: '',
+    mobile: '',
+    email: '',
+    address: '',
+    birthday: '2015-12-31',
+    sex: null,
+    introduction: '',
+  });
+
+  const [passwordInput, setPasswordInput] = useState({
+    oldPassword: '',
+    newPassword: '',
+    confirmPassword: '',
+  });
+  const [isEqual, setIsEqual] = useState(false);
+  const [isLoadingDataFromDb, setIsLoadingDataFromDb] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsEqual(
+      !passwordInput.newPassword.localeCompare(passwordInput.confirmPassword)
+        ? true
+        : false
+    );
+  }, [passwordInput.newPassword, passwordInput.confirmPassword]);
+  useEffect(() => {
+    setIsLoadingDataFromDb(true);
+    getCollectorInformation().then((response) => {
+      if (response.status === 200) {
+        setProfileInput({
+          fullname: response.data.full_name || '',
+          mobile: response.data.mobile || '',
+          email: response.data.email || '',
+          address: response.data.address || '',
+          birthday: response.data.birth_date || '2015-12-31',
+          sex: String(response.data.sex !== null ? response.data.sex : '0'),
+          introduction: response.data.introduction || '',
+        });
+      }
+      setIsLoadingDataFromDb(false);
+    });
+  }, []);
+
+  const onChangeProfileInput = (e) => {
+    setProfileInput({
+      ...profileInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onChangePasswordInput = (e) => {
+    setPasswordInput({
+      ...passwordInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
     if (!e.target.checkValidity()) {
-      setIsValidated(true);
+      return;
+    }
+    setIsValidated(true);
+
+    if (!isChangingPassword) {
+      setIsLoading(true);
+      updateCollectorInformation({
+        fullname: profileInput.fullname,
+        mobile: profileInput.mobile,
+        email: profileInput.email,
+        address: profileInput.address,
+        birth_date: profileInput.birthday,
+        sex: profileInput.sex,
+        introduction: profileInput.introduction,
+      }).then((response) => {
+        setIsLoading(false);
+        if (response.status === 200) {
+          Swal.fire('Good job!', response.data.message, 'success');
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: response.data.message,
+          });
+        }
+      });
+    }
+
+    if (isChangingPassword && isEqual) {
+      setIsLoading(true);
+      const data = {
+        oldPassword: passwordInput.oldPassword,
+        newPassword: passwordInput.newPassword,
+      };
+      updateCollectorInformation(data).then((response) => {
+        setIsLoading(false);
+        if (response.status === 200) {
+          Swal.fire('Good job!', response.data.message, 'success');
+          setPasswordInput({
+            oldPassword: '',
+            newPassword: '',
+            confirmPassword: '',
+          });
+          setIsValidated(false);
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text:
+              response.status !== 500
+                ? response.data.message
+                : 'Something went wrong',
+          });
+        }
+      });
     }
   };
+
   return (
     <>
+      {isLoading && <Modal children={<Loader />} />}
+      {isLoadingDataFromDb && <Loader />}
       <div className="d-flex align-items-center justify-content-between">
-        <h3>Thông tin cá nhân</h3>
+        <h3>
+          {!isChangingPassword ? 'Thông tin cá nhân' : 'Thay đổi mật khẩu'}
+        </h3>
         <button
           className="btn btn-primary text-white"
           style={{ width: '180px' }}
@@ -21,7 +142,7 @@ export default function UserProfile() {
           {isChangingPassword ? 'Thông tin cá nhân' : 'Thay đổi mật khẩu'}
         </button>
       </div>
-      {!isChangingPassword && (
+      {!isLoadingDataFromDb && !isChangingPassword && (
         <form
           className={`row g-3 needs-validation`}
           noValidate
@@ -34,8 +155,11 @@ export default function UserProfile() {
             <input
               type="text"
               className="form-control"
+              name="fullname"
               id="fullname"
               placeholder="Họ và tên..."
+              value={profileInput.fullname}
+              onChange={onChangeProfileInput}
             />
           </div>
 
@@ -46,8 +170,11 @@ export default function UserProfile() {
             <input
               type="text"
               className="form-control"
+              name="mobile"
               id="mobile"
               placeholder="Số điện thoại..."
+              value={profileInput.mobile}
+              onChange={onChangeProfileInput}
             />
           </div>
 
@@ -58,41 +185,57 @@ export default function UserProfile() {
             <input
               type="email"
               className="form-control"
+              name="email"
               id="email"
               placeholder="Địa chỉ Email..."
+              value={profileInput.email}
+              onChange={onChangeProfileInput}
             />
           </div>
 
           <div className="col-md-8">
             <label htmlFor="address" className="form-label">
-              Address
+              Địa chỉ nhà
             </label>
             <input
               type="text"
               className="form-control"
+              name="address"
               id="address"
-              placeholder="Address..."
+              placeholder="Địa chỉ..."
+              value={profileInput.address}
+              onChange={onChangeProfileInput}
             />
           </div>
 
           <div className="col-md-4">
             <label htmlFor="date" className="form-label">
-              Birthday
+              Ngày sinh
             </label>
-            <input type="date" className="form-control" id="date" />
+            <input
+              type="date"
+              className="form-control"
+              name="birthday"
+              id="date"
+              value={profileInput.birthday}
+              onChange={onChangeProfileInput}
+            />
           </div>
 
           <div>
-            <p className="mb-1">Sex</p>
+            <p className="mb-1">Giới tính</p>
             <div className="form-check">
               <input
                 className="form-check-input"
                 type="radio"
                 name="sex"
                 id="male"
+                value={0}
+                onChange={onChangeProfileInput}
+                defaultChecked={profileInput.sex === '0' ? true : false}
               />
               <label className="form-check-label" htmlFor="male">
-                Male
+                Nam
               </label>
             </div>
             <div className="form-check">
@@ -100,36 +243,41 @@ export default function UserProfile() {
                 className="form-check-input"
                 type="radio"
                 name="sex"
+                value={1}
                 id="female"
-                defaultChecked
+                onChange={onChangeProfileInput}
+                defaultChecked={profileInput.sex === '0' ? false : true}
               />
               <label className="form-check-label" htmlFor="female">
-                Female
+                Nữ
               </label>
             </div>
           </div>
 
           <div className="col-md-12">
             <label htmlFor="introduction" className="form-label">
-              Introduction
+              Giới thiệu
             </label>
             <textarea
               type="text"
               className="form-control w-100"
+              name="introduction"
               id="introduction"
-              placeholder="Some information about yourself..."
+              placeholder="Một vài thông tin về bản thân..."
               style={{ height: '150px' }}
+              onChange={onChangeProfileInput}
+              value={profileInput.introduction}
             />
           </div>
           <div className="col-12">
             <button className="btn btn-primary" type="submit">
-              Submit form
+              Cập nhật
             </button>
           </div>
         </form>
       )}
 
-      {isChangingPassword && (
+      {!isLoadingDataFromDb && isChangingPassword && (
         <form
           className={`row g-3 needs-validation ${
             isValidated ? 'was-validated' : ''
@@ -146,6 +294,9 @@ export default function UserProfile() {
               className="form-control"
               id="oldPassword"
               placeholder="Nhập mật khẩu cũ..."
+              name="oldPassword"
+              value={passwordInput.oldPassword}
+              onChange={onChangePasswordInput}
               required
             />
             <div className="invalid-feedback">
@@ -161,6 +312,9 @@ export default function UserProfile() {
               className="form-control"
               id="newPassword"
               placeholder="Nhập mật khẩu mới..."
+              name="newPassword"
+              value={passwordInput.newPassword}
+              onChange={onChangePasswordInput}
               required
             />
             <div className="invalid-feedback">
@@ -172,10 +326,13 @@ export default function UserProfile() {
               Xác nhận mật khẩu
             </label>
             <input
-              type="text"
+              type="password"
               className="form-control"
               id="confirmPassword"
               placeholder="Xác nhận khẩu mới..."
+              name="confirmPassword"
+              value={passwordInput.confirmPassword}
+              onChange={onChangePasswordInput}
               required
             />
             <div className="invalid-feedback">
@@ -185,7 +342,7 @@ export default function UserProfile() {
 
           <div className="col-12">
             <button className="btn btn-primary" type="submit">
-              Submit form
+              Đổi mật khẩu
             </button>
           </div>
         </form>
