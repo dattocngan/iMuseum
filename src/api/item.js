015474d36e5c6cfd5eb8a9cd8ef4870b1988ecd9
Dat Nguyen 184 <dattocngan@gmail.com>
2022-10-06 11:40:22 +0700

    add all modules for Item

--- src/api/item.js
+++ src/api/item.js
@@ -18,6 +18,33 @@ export const addItem = async (data) => {
     }
 };
 
+export const getItem = async (id) => {
+    try {
+        const response = await axios.get(`items/${id}`);
+        return response;
+    } catch (err) {
+        return err.response;
+    }
+};
+
+export const updateItem = async (id, data) => {
+    try {
+        const response = await axios.put(`items/${id}`, data);
+        return response;
+    } catch (err) {
+        return err.response;
+    }
+};
+
+export const deleteImages = async (id, data) => {
+    try {
+        const response = await axios.delete(`items/${id}/images`, {data: data});
+        return response;
+    } catch (err) {
+        return err.response;
+    }
+}
+
 export const getAges = async () => {
     try {
         const response = await axios.get(`ages`);
--- src/layouts/Admin/Items/Items.js
+++ src/layouts/Admin/Items/Items.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
 import AllItems from "../../../views/Items/AllItems";
 import AddItem from "../../../views/Items/AddItem/AddItem";
+import Item from "../../../views/Items/Item/Item";
 
 const Items = () => {
     return (
         <Switch>
             <Route path="/admin/items" component={AllItems} exact />
             <Route path='/admin/items/add' component={AddItem} exact />
+            <Route path='/admin/items/:id' component={Item} exact />
             <Redirect to="/admin/items" />
         </Switch>
     );
